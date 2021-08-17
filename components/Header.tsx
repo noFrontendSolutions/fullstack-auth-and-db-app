e98e3c3e22089b61997f70976469700444f086b5
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-17 14:24:36 +0200

    show e-mail after login

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
 
     return(
         <nav className = "flex flex-row justify-between p-6">
-            <div>
+            <div className = "flex items-center">
             <Link href = "/">
                 <a className = {asPath === "/" ? "m-4 border-gray-700 border-b-4" : "m-4"}>Home</a>
             </Link>
@@ -21,20 +21,21 @@ const Header = () => {
             </Link>
             </div>
             { !context.user &&
-            <div>
+            <div className = "m-4">
                 <button onClick = {context.login}>
                     Sign In / Login
                 </button>
             </div>
             }
             { context.user &&
-            <div>
+            <div className = "flex items-center">
                 <Link href = "/NewBlog">
                 <a className = {asPath === "/NewBlog" ? "m-4 border-gray-700 border-b-4" : "m-4"}>Create New Blog</a>
                 </Link>
                 <Link href = "/MyContributions">
                 <a className = {asPath === "/MyContributions" ? "m-4 border-gray-700 border-b-4" : "m-4"}>My Contributions</a>
                 </Link>
+                <div className = "mx-4 px-2 bg-gray-300 rounded">{context.user}</div>
                 <button onClick = {context.logout}>
                     Logout
                 </button>
