ae366e400b5b943b9588a5c32b928049e7eb06c6
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-22 19:56:48 +0200

    dropped netlify for auth. Now working with auth0

--- database/db-related.js
+++ database/db-related.js
@@ -1,54 +1,42 @@
 import { MongoClient } from "mongodb"
 import { ObjectID } from "bson"
-let cachedData = {}
+//let cachedData = {}
+
+const connection = async () => {
+  const MONGO_URI = process.env.DB_HOST
+  const client = await MongoClient.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    return client
+}
 
 
 export const connectToDB = async () => {
-    const MONGO_URI = process.env.DB_HOST
-    const client = await MongoClient.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-    const db = client.db()
+    const connec = await connection()
+    const db = connec.db()
     const articleCollection = db.collection("articles")
     const allArticles = await articleCollection.find().toArray()
-    client.close()
+    connec.close()
     return allArticles
 }
 
 export const findDBArticle = async (id) => {
-    const MONGO_URI = process.env.DB_HOST
-    const client = await MongoClient.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-    const db = client.db()
+    const db = (await connection()).db()
     const articleCollection = db.collection("articles")
     const objId = new ObjectID(id)
     const article = await articleCollection.find({"_id": objId}).toArray()
-    client.close()
     return article
 }
 
 export const submitArticleToDB = async (article) => {
-    const MONGO_URI = process.env.DB_HOST
-    const client = await MongoClient.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-    const db = client.db()
+    const db = (await connection()).db()
     const articleCollection = db.collection("articles")
     await articleCollection.insertOne(article)
-    client.close()
 }
 
 export const updateDB = async (id, article) => {
-    const MONGO_URI = process.env.DB_HOST
-    const client = await MongoClient.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-    const db = client.db()
+    const db = (await connection()).db()
     const articleCollection = db.collection("articles")
     const objId = new ObjectID(id)
     await articleCollection.updateOne({"_id": objId}, {$set: {title: article.title, description: article.description, content: article.content}}, { upsert: false })
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import 'tailwindcss/tailwind.css'
 import Layout from "../components/Layout"
 import {AuthContextProvider} from "../authentication/AuthContext"
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }) {
   return (
+   
     <AuthContextProvider>
+      <UserProvider>
       <Layout>
           <Component {...pageProps} />
       </Layout>
+      </UserProvider>
     </AuthContextProvider>
   )
 }
