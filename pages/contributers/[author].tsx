e8f6c34f0f0fdf459fe255eeeb5090f919dc4119
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-25 23:51:05 +0200

    fixed link to author's articles

--- pages/contributers/[author].tsx
+++ pages/contributers/[author].tsx
@@ -1,13 +1,11 @@
 import { connectToDB, getDBAuthors, getAuthorsDBArticles } from "../../database/db-related";
 import Link from "next/link";
 
-export interface Article {
+export interface ArticleCard {
   _id: string;
   author: string;
-  email: string;
   title: string;
   description: string;
-  markdown: string;
   date: string;
 }
 
@@ -36,12 +34,11 @@ export const getStaticProps = async (context: any) => {
   data.filter(article => authorUrl === urlIt(article.author))
   let articles = data.map( article => {
     return {
-    id_: article._id.toString(),
+    _id: article._id.toString(),
     author: article.author,
-    email: article.email,
     title: article.title,
     description: article.description,
-    markdown: article.markdown,
+    date: article.date
   }
 })
   return {
@@ -49,11 +46,11 @@ export const getStaticProps = async (context: any) => {
   };
 };
 
-const Contributions: React.FC<{articles: Article[]}> = (props) => {
+const Contributions: React.FC<{articles: ArticleCard[]}> = (props) => {
   return (
     <div className="min-h-full p-4 overflow-scroll">
-      {props.articles.map((article: Article) => (
-        <Link key={article._id} href={"/articles/" + article._id}>
+      {props.articles.map((article) => (
+        <Link key={article._id} href={"../articles/" + article._id}>
           <h2 className="text-blue-500 font-bold text-2xl text-center>">
             {article.title}
           </h2>
