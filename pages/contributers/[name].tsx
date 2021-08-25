f888038f0a82e260830e8278f427660b5dec6cf1
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-25 18:39:57 +0200

    added static paths to contributers folder

--- pages/contributers/[name].tsx
+++ pages/contributers/[name].tsx
@@ -1,17 +1,31 @@
+import { connectToDB } from "../../database/db-related";
 
+const urlIt = (string: string) => {
+  string = string.trim().toLowerCase().split(" ").join("-");
+  return string;
+};
+
+export const getStaticPaths = async () => {
+  let authors: string[];
+  const allArticles = await connectToDB();
+  allArticles.forEach((article) => {
+    if (authors.includes(article.author)) {
+      authors.push(article.author);
+    }
+  });
+
+  const paths = authors!.map((author) => {
+    return {
+      params: { name: urlIt(author) },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 const Contributer = () => {
-    
-
-    const nameToUrl = (contributer: string) => {
-        contributer = contributer.trim().split(" ").join("-");
-        return contributer;
-      };
-    return(
-        <div>
-            
-        </div>
-    )
-      
-}
-export default Contributer
+  return <div></div>;
+};
+export default Contributer;
