830bbe1c7290c26c13ca9cf75a3645ebcfedccca
noFrontendSolutions <b.welzel81@gmail.com>
2021-08-30 13:53:58 +0200

    added styles to InputForm component

--- components/InputForm.tsx
+++ components/InputForm.tsx
@@ -28,19 +28,16 @@ const InputOutput: React.FC<Submission> = (props) => {
   const user = useUser();
 
   return (
-    <div className="h-full p-4">
+    <div className="min-h-full p-4 grid grid-cols-1 w-full xl:w-1/2 self-center">
       {!user && (
         <div className="flex items-center justify-center text-2xl">
-          YOU'RE NOT AUTHORIZED TO VIEW THIS CONTENT. LOGIN BEFORE YOU START
-          WRITING A NEW BLOG!
+          YOU'RE NOT AUTHORIZED TO VIEW THIS CONTENT!
         </div>
       )}
       {user && (
         <>
-          <div className="fixed w-1/2 min-h-full">
-            <div className="">
               <form className="flex flex-col border p-2">
-                <label htmlFor="input">Title</label>
+                <label htmlFor="input" className = "font-bold text-gray-700">Card Title:</label>
                 <input
                   spellCheck="false"
                   className="border p-2"
@@ -48,7 +45,7 @@ const InputOutput: React.FC<Submission> = (props) => {
                   onChange={(e) => props.setTitle(e.target.value)}
                   defaultValue={props.title ? props.title : ""}
                 ></input>
-                <label htmlFor="input">Image Url</label>
+                <label htmlFor="input" className = "font-bold text-gray-700">Card Image Url:</label>
                 <input
                   spellCheck="false"
                   className="border p-2"
@@ -56,57 +53,55 @@ const InputOutput: React.FC<Submission> = (props) => {
                   onChange={(e) => props.setImageUrl(e.target.value)}
                   defaultValue={props.imageUrl ? props.imageUrl : ""}
                 ></input>
-                <label htmlFor="textarea">Description</label>
+                <label htmlFor="textarea" className = "font-bold text-gray-700">Card Description:</label>
                 <textarea
                   placeholder="Start Here..."
                   className="border h-48 p-2"
                   onChange={(e) => props.setDescription(e.target.value)}
                   defaultValue={props.description ? props.description : ""}
                 ></textarea>
-                <label htmlFor="textarea">Main Content (Markdown)</label>
+                <label htmlFor="textarea" className = "font-bold text-gray-700">Article Content (Markdown):</label>
                 <textarea
                   spellCheck="false"
-                  placeholder="Start Here..."
-                  className="border h-96 p-2"
+                  placeholder=""
+                  className="border h-96 p-2 font-mono bg-gray-700 text-white"
                   onChange={(e) => props.setMarkdown(e.target.value)}
                   defaultValue={props.markdown ? props.markdown : ""}
                 ></textarea>
               </form>
               {!props.edit && (
                 <button
-                  className="my-2 p-2 border-2 rounded full bg-red-400 text-gray-700 font-bold"
+                  className="my-2 p-2 border-2 text-white bg-indigo-500 rounded-xl font-bold"
                   onClick={props.submitHandler}
                 >
                   SUBMIT!
                 </button>
               )}
               {props.edit && (
-                <span>
+                <div className = "w-full flex justify-between">
                   <button
-                    className="my-2 p-2 border-2 rounded full bg-green-400 text-gray-700 font-bold"
+                    className="my-2 p-2 border-2 text-white bg-green-500 rounded-xl font-bold w-48"
                     onClick={props.resubmitHandler}
                   >
-                    RESUBMIT!
+                    RESUBMIT ARTICLE
                   </button>
                   <button
-                    className="my-2 p-2 border-2 rounded full bg-red-400 text-gray-700 font-bold"
+                    className="my-2 p-2 border-2 text-white bg-red-500 rounded-xl font-bold w-48"
                     onClick={props.deleteHandler}
                   >
-                    DELETE!
+                    DELETE ARTICLE
                   </button>
-                </span>
+                </div>
               )}
-            </div>
-          </div>
-          <div className="mt-8 ml-4 min-h-full absolute right-8 min-w-1/2 ">
-            <label htmlFor="input" className="font-bold">
+          
+            <label htmlFor="input" className="mt-8 font-bold text-lg">
               Markdown Preview
             </label>
             <div
-              className="h-full ml-8 p-2 prose prose-sm sm:prose lg:prose-lg xl:prose-xl"
+              className="pt-8 prose prose-sm sm:prose lg:prose-lg xl:prose-xl border-t-4 self-center"
               dangerouslySetInnerHTML={renderCleanMarkdown(props.markdown)} //There was no way to get ReactMarkdown to work in combination with getStaticProps (at least I think that this was causing the webpack error). So there's no way around "dangerouslySetInnerHTML". I hope the sanitize function (createCleanMArkdown()) does the trick.
             ></div>
-          </div>
+        
         </>
       )}
     </div>
--- pages/my-contributions.tsx
+++ pages/my-contributions.tsx
@@ -1,70 +1,76 @@
-import {connectToDB} from "../database/db-related"
-import Link from "next/link"
-import { GetStaticProps } from "next"
-import { useUser } from '@auth0/nextjs-auth0'
-
+import { connectToDB } from "../database/db-related";
+import Link from "next/link";
+import { GetStaticProps } from "next";
+import { useUser } from "@auth0/nextjs-auth0";
 
 interface ArticleCard {
-    _id: string
-    author: string
-    email: string   
-    title: string
-    description: string
-    date: string
-    imageUrl: string
+  _id: string;
+  author: string;
+  email: string;
+  title: string;
+  description: string;
+  date: string;
+  imageUrl: string;
 }
-
 
 export const getStaticProps: GetStaticProps = async () => {
-    const data = await connectToDB()
-    const collection: ArticleCard[] = data.map(article => {
-        const id = JSON.parse(JSON.stringify(article._id))
-        return {
-            _id: id,
-            author: article.author,
-            email: article.email,
-            title: article.title,
-            description: article.description,
-            date: article.date,
-            imageUrl: article.imageUrl
-        }
-    })
-    
+  const data = await connectToDB();
+  const collection: ArticleCard[] = data.map((article) => {
+    const id = JSON.parse(JSON.stringify(article._id));
     return {
-        props: {collection}
-      }
-  }
+      _id: id,
+      author: article.author,
+      email: article.email,
+      title: article.title,
+      description: article.description,
+      date: article.date,
+      imageUrl: article.imageUrl,
+    };
+  });
 
-const MyContributions: React.FC<{collection: ArticleCard[]}> = (props) => {
-   
+  return {
+    props: { collection },
+  };
+};
 
-    const {user} = useUser()
-    let myArticles: ArticleCard[] = props.collection.filter(article => article.email === user?.email) //I'm identifying author's contributions via email because "user.name" (from auth0 user profile) could be saved differently (trimmed) in Mongo Atlas, hence "user.name === article.author" wouldn't hold anymore...hence you get zero matches  
+const MyContributions: React.FC<{ collection: ArticleCard[] }> = (props) => {
+  const { user } = useUser();
+  let myArticles: ArticleCard[] = props.collection.filter(
+    (article) => article.email === user?.email
+  );
 
-    return (
-        <div className = "h-screen grid grid-cols-1 self-center">
-        { !user &&
-        <div className = "h-full flex items-center justify-center text-3xl">
-            YOU'RE NOT AUTHORIZED TO VIEW THIS CONTENT!            
+  return (
+    <div className="h-screen grid grid-cols-1 self-center">
+      {!user && (
+        <div className="h-full flex items-center justify-center text-3xl">
+          YOU'RE NOT AUTHORIZED TO VIEW THIS CONTENT!
         </div>
-        }
-        { user &&
-        <div className = "h-full relative">
-            {myArticles.map(article => (
-                <Link key = {article._id} href = {"/edit-article/" + article._id}>
-                    <a>
-                    <div key = {article._id} className = "m-4 p-4 border rounded-xl shadow-lg hover:border-blue-500 relative max-w-4xl">
-                    <img src = "/edit.png" className = "h-8 w-8 absolute right-2 top-2 object-cover"></img>
-                        <h2 className = "mb-2 font-bold text-xl text-center>">{article.title}</h2>
-                         <p className = "m-2">{article.description}</p>
-                    </div>
-                </a>
-                </Link>
-            ))}
-         </div>
-        }
+      )}
+      {user && (
+        <div title="Edit Article" className="h-full relative">
+          {myArticles.map((article) => (
+            <Link key={article._id} href={"/edit-article/" + article._id}>
+              <a>
+                <div
+                  key={article._id}
+                  className="m-4 p-4 border rounded-xl shadow-lg hover:border-blue-500 relative max-w-4xl"
+                >
+                  <img
+                    src="/edit.png"
+                    className="h-8 w-8 absolute right-2 top-2 object-cover"
+                  ></img>
+                  <h2 className="mb-2 font-bold text-xl text-center>">
+                    {article.title}
+                  </h2>
+                  <p className="m-2">{article.description}</p>
+                </div>
+              </a>
+            </Link>
+          ))}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default MyContributions
+export default MyContributions;
