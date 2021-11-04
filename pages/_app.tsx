ef8be9575e2c44d650ced04130992ce28f2edf5f
noFrontendSolutions <b.welzel81@gmail.com>
2021-11-04 13:03:09 +0100

    removed unused code and modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,7 @@ import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { AppProps } from "next/dist/next-server/lib/router/router";
-import { useState} from "react"
-import { QueryClient, QueryClientProvider } from 'react-query'
+
 
 interface Article {
   _id: string
@@ -15,24 +14,16 @@ interface Article {
   imageUrl: string
 }
 
-//let articles: Article[] = []
-const queryClient = new QueryClient()
-//context provider obviously does nothing at the moment...Might come back to it later!
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   
 
   return (
-    <QueryClientProvider client={queryClient}>
     <UserProvider>
-      
         <Layout>
          <Component {...pageProps} />
        </Layout>
-      
     </UserProvider>
-    </QueryClientProvider>
   );
 }
 
